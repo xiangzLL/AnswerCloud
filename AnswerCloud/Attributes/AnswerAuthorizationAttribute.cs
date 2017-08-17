ba172fd7dd97b0d403a73b0128db805dd673b8e4
xiangz <751573533@qq.com>
2017-08-17 16:26:19 +0800

    commit code

---
+++ AnswerCloud/Attributes/AnswerAuthorizationAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AnswerCloud.Attributes
+{
+    public class AnswerAuthorizationAttribute:AuthorizeAttribute
+    {
+        public override void OnAuthorization()
+    }
+}
